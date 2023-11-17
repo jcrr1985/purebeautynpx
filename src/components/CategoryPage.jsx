ba133e292347905790fe3a71555c29781e0328e3
julio rodriguez <julio.rodriguez-ext@talan.com>
2023-11-17 13:19:11 +0100

    categories inline

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import Navbar from "./Navbar";
-import ItemElement from "./ItemElement";
-import itemsData from "./itemsData";
-import { Grid, Paper } from "@mui/material";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Navbar from './Navbar';
+import ItemElement from './ItemElement';
+import itemsData from './itemsData';
+import { Grid, Paper } from '@mui/material';
 
 const CategoryPage = ({ addToCart }) => {
   const { category } = useParams();
   const filteredEntities =
-    category === "all-items"
+    category === 'all-items'
       ? itemsData
       : itemsData.filter((item) => item.name.toLowerCase() === category);
 
@@ -16,31 +16,49 @@ const CategoryPage = ({ addToCart }) => {
 
   return (
     <div className="cart-page-wrapper">
-      <Grid container spacing={3}>
-        <div className="left-cp">
-          <Grid item xs={12} md={4} lg={3}>
-            <Navbar selectedCategoria={category} />
-          </Grid>
-        </div>
-        <div className="right-cp">
-          <Grid item xs={12}>
-            <Grid container spacing={3}>
-              {entityItems.flat().map((item, index) => (
-                <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
-                  <ItemElement
-                    key={item.id}
-                    imageSrc={item.imageSrc}
-                    name={item.name}
-                    price={item.price}
-                    materials={item.materials}
-                    addToCart={() => addToCart(item, "add")}
-                    id={item.id}
-                    item={item}
-                  />
-                </Grid>
-              ))}
+      <Grid
+        container
+        spacing={3}>
+        <div className="all-cp">
+          <div className="left-cp">
+            <Grid
+              item
+              xs={12}
+              md={4}
+              lg={3}>
+              <Navbar selectedCategoria={category} />
             </Grid>
-          </Grid>
+          </div>
+          <div className="right-cp">
+            <Grid
+              item
+              xs={12}>
+              <Grid
+                container
+                spacing={3}>
+                {entityItems.flat().map((item, index) => (
+                  <Grid
+                    item
+                    key={index}
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    lg={3}>
+                    <ItemElement
+                      key={item.id}
+                      imageSrc={item.imageSrc}
+                      name={item.name}
+                      price={item.price}
+                      materials={item.materials}
+                      addToCart={() => addToCart(item, 'add')}
+                      id={item.id}
+                      item={item}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </div>
         </div>
       </Grid>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import categoriesList from "./itemsData";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import categoriesList from './itemsData';
 
 const Navbar = ({ selectedCategoria }) => {
-  const [selectedCategory, setSelectedCategory] = useState("All Items");
+  const [selectedCategory, setSelectedCategory] = useState('All Items');
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
@@ -11,30 +11,28 @@ const Navbar = ({ selectedCategoria }) => {
 
   return (
     <div className="navbar">
-      <div style={{ width: "max-content" }}>
+      <div style={{ width: 'max-content' }}>
         <h4 className="navbar-title">Browse by Categories</h4>
-      </div>{" "}
+      </div>{' '}
       <div className="heart-list-container">
         <ul className="heart-list">
           <li
-            onClick={() => handleCategoryClick("all-items")}
+            onClick={() => handleCategoryClick('all-items')}
             className={
-              selectedCategoria === "all-items" ? "active-categorie" : ""
-            }
-          >
-            <Link to="/category/all-items">All Items</Link>
+              selectedCategoria === 'all-items' ? 'active-categorie' : ''
+            }>
+            <Link to="/category/all-items">All &nbsp;Items</Link>
           </li>
           {categoriesList &&
             categoriesList.map((category) => (
               <li
                 key={category.name}
-                onClick={() => handleCategoryClick("all-items")}
+                onClick={() => handleCategoryClick('all-items')}
                 className={
                   selectedCategoria === category.name.toLowerCase()
-                    ? "active-categorie"
-                    : ""
-                }
-              >
+                    ? 'active-categorie'
+                    : ''
+                }>
                 <Link to={`/category/${category.name.toLowerCase()}`}>
                   {category.name}
                 </Link>

7c31a3602aec24ff5b19c0202020181f3730905b
julio rodriguez <julio.rodriguez-ext@talan.com>
2023-11-26 00:51:05 +0100

    hidding magnifying glass on last step

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,26 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import Select from 'react-select';
-import { categoriesList } from './itemsData';
+import React, { useEffect, useState } from 'react'
+import Select from 'react-select'
+import { categoriesList } from './itemsData'
 
 const SearchBar = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(null)
 
   const options = categoriesList.flatMap((category) =>
-    category.items.map((item) => ({ value: item.id, label: item.name }))
-  );
+    category.items.map((item) => ({ value: item.id, label: item.name })),
+  )
 
   const handleChange = (selectedOption) => {
-    setSelectedOption(selectedOption);
+    setSelectedOption(selectedOption)
     // Aquí puedes realizar acciones adicionales según la opción seleccionada
-  };
+  }
 
   return (
     <Select
-      className="search-input"
+      className='search-input'
       value={selectedOption}
       onChange={handleChange}
       options={options}
-      placeholder="Search..."
+      placeholder='Search...'
+      MenuProps={{
+        PaperProps: {
+          sx: {
+            bgcolor: 'pink',
+            '& .MuiMenuItem-root': {
+              padding: 2,
+              zIndex: 99999999999,
+            },
+          },
+        },
+      }}
       styles={{
         control: (base) => ({
           ...base,
@@ -29,7 +40,7 @@ const SearchBar = () => {
         }),
       }}
     />
-  );
-};
+  )
+}
 
-export default SearchBar;
+export default SearchBar

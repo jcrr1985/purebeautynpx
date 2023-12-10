7421fd593e2f804bf209e902483cd0d607feda2a
julio rodriguez <julio.rodriguez-ext@talan.com>
2023-12-10 03:59:37 +0100

    loging workig

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const cors = require('cors');
-const Stripe = require('stripe');
+require('dotenv').config()
+const cors = require('cors')
+const Stripe = require('stripe')
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 app.post('/api/checkout', async (req, res) => {
   try {
-    const { id, amount, dataForm } = req.body;
-    console.log('🚀 ~ file: server.js:23 ~ app.post ~ data', dataForm);
+    const { id, amount, dataForm } = req.body
+    console.log('🚀 ~ ~ app.post ~ data', dataForm)
     const payment = await stripe.paymentIntents.create({
       amount,
       currency: 'USD',
@@ -18,17 +18,17 @@ app.post('/api/checkout', async (req, res) => {
       payment_method: id,
       confirm: true,
       return_url: 'http://localhost:3000/success',
-    });
-    console.log('🚀 ~ SUCCESSFUL PAYMENT', payment);
-    res.send({ message: 'Successful payment' });
+    })
+    console.log('🚀 ~ SUCCESSFUL PAYMENT', payment)
+    res.send({ message: 'Successful payment' })
   } catch (error) {
-    console.log('🚀 ~ app.post ~ error:', error);
+    console.log('🚀 ~ app.post ~ error:', error)
   }
-});
+})
 
 const privateKey =
-  'pk_test_51NmKBUIyGuUAStfNoHpVSC7wjVBwuo8dMuGBe4c4H6z52EdTfdD2XBypC6B3naKeL01K0hVJ3bs45zADZNHSBaZM00UWQtptaZ';
+  'pk_test_51NmKBUIyGuUAStfNoHpVSC7wjVBwuo8dMuGBe4c4H6z52EdTfdD2XBypC6B3naKeL01K0hVJ3bs45zADZNHSBaZM00UWQtptaZ'
 
-app.listen(3001, () => console.log('Listening on port 3001'));
+app.listen(3001, () => console.log('Listening on port 3001'))
 
-const stripe = new Stripe(privateKey);
+const stripe = new Stripe(privateKey)
